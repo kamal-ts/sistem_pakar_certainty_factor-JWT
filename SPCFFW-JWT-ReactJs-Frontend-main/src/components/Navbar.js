5a4bff4136f7f829aba242a26fa84d2ff1ca0098
kamal1234567890 <kamaluddin1623@gmail.com>
2024-11-22 01:28:31 +0900

    create link in header

--- SPCFFW-JWT-ReactJs-Frontend-main/src/components/Navbar.js
+++ SPCFFW-JWT-ReactJs-Frontend-main/src/components/Navbar.js
@@ -66,9 +66,9 @@ const Navbar = () => {
 
         <div className="container-xxl position-relative p-0">
             <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-                <a href="index.html" className="navbar-brand p-0">
+                <Link to={'/'} className="navbar-brand p-0">
                     <h1 className="m-0">CFconsult</h1>
-                </a>
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span className="fa fa-bars"></span>
                 </button>
